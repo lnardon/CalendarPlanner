fd57ae6c7ee6432904da396f7849d362c05bb95f
lnardon <lucaslnardon@gmail.com>
2020-11-19 02:05:00 -0300

    🚀 Add githubpages website config

---
+++ docs/precache-manifest.4ab2103d6d679846eaa309e6d6897f49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d07a14f36b85203a4228f2a2a5c61f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b131fdd6732fd7c23857",
+    "url": "/static/css/main.e7a7ef83.chunk.css"
+  },
+  {
+    "revision": "4ae1db914d987f44b33d",
+    "url": "/static/js/2.d5208a63.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d5208a63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b131fdd6732fd7c23857",
+    "url": "/static/js/main.954dee24.chunk.js"
+  },
+  {
+    "revision": "e796542ebc12ae304c4f",
+    "url": "/static/js/runtime-main.36dc1e0b.js"
+  }
+]);
