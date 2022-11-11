c8b4b7eb8a3beb8147699733cbdbba6abc91218d
Lucas Nardon <lucaslnardon@gmail.com>
2022-11-10 23:15:34 -0300

    ✨ Add databse connection

--- firebase.js
+++ firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_GOOGLE_API_KEY,
@@ -8,7 +8,8 @@ const firebaseConfig = {
   storageBucket: process.env.NEXT_PUBLIC_GOOGLE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_GOOGLE_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_GOOGLE_APP_ID,
+  databaseURL: process.env.NEXT_PUBLIC_DATABASE_URL,
 };
 
 export const app = initializeApp(firebaseConfig);
-// export const analytics = getAnalytics(app);
+export const database = getDatabase(app);
